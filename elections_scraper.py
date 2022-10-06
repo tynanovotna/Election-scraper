cc162dd3c918814d346f0f758005230815df36e7
Kristýna Novotná <tynadanielova@seznam.cz>
2022-10-06 14:28:44 -0700

    Working code

--- elections_scraper.py
+++ elections_scraper.py
@@ -6,15 +6,13 @@ email: tynadanielova@seznam.cz
 discord: KristýnaN #4503
 """
 
-import bs4
 from bs4 import BeautifulSoup
-import html5lib
-import lxml
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
 import re
 import sys
+import csv
 
 URL_BEGINNIG = "https://www.volby.cz/pls/ps2017nss/"
 
@@ -45,28 +43,59 @@ def validate_output_file_name(output_file_name):
         print("Incorect file suffix(= .csv). ")
         exit()
 
-def process_data(region_urls):
-    processed_data = {}
-    for region_url in region_urls:
-        response = download_web_page(region_url)
-        soup = BeautifulSoup(response.content, "lxml")
-        town_code = re.search("xobec=(.*?)&x", region_url).group(1)
-        if not town_code in processed_data:
-            data = soup.find(id="publikace")
-            h3 = data.find_all("h3")
-            town = h3[2].text.split("Obec: ")[1].strip()
-            tables = soup.find_all("table")
-            table_data = tables[0].find_all("td")
-            registered = table_data[3].text.replace("\xa0", " ")
-            envelopes = table_data[4].text.replace("\xa0", " ")
-            valid = table_data[7].text.replace("\xa0", " ")        
-        
+def process_region_data(region_url):
+    region_data = {}
+    response = download_web_page(region_url)
+    soup = BeautifulSoup(response.content, "lxml")
+    region_data["code"] = re.search("xobec=(.*?)&x", region_url).group(1)
+    data = soup.find(id="publikace")
+    h3 = data.find_all("h3")
+    region_data["location"] = h3[2].text.split("Obec: ")[1].strip()
+    tables = soup.find_all("table")
+    table_data = tables[0].find_all("td")
+    region_data["registered"] = table_data[3].text.replace("\xa0", " ")
+    region_data["envelopes"] = table_data[4].text.replace("\xa0", " ")
+    region_data["valid"] = table_data[7].text.replace("\xa0", " ")
+    
+    party_names_with_votes = {}
+    for i in range(1, len(tables)):
+        table = tables[i]
+        party_names = table.find_all(headers=f"t{i}sa1 t{i}sb2")
+        valid_votes = table.find_all(headers=f"t{i}sa2 t{i}sb3")
+        for j in range(len(party_names)):
+            if not party_names[j].text == "-":
+                party_names_with_votes[party_names[j].text] = valid_votes[j].text.replace("\xa0", " ")
+    region_data["party_names_with_votes"] = party_names_with_votes
+    return region_data
+
+def process_data_and_make_file(region_urls, output_file_name):
+    party_names = []
+    print(f"Writing file {output_file_name}")
+    with open(output_file_name, "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter=";")
+        for region_url in region_urls:
+            region_data = process_region_data(region_url)
+            if not party_names:
+                party_names = list(region_data["party_names_with_votes"].keys())
+                writer.writerow(["Code", "Location", "Registred", "Envelopes", "Valid"] + party_names)
+            party_votes = []
+            for party_name in party_names:
+                party_votes.append(region_data["party_names_with_votes"][party_name])
+            writer.writerow([
+                region_data["code"],
+                region_data["location"], 
+                region_data["registered"],
+                region_data["envelopes"], 
+                region_data["valid"]
+            ] + party_votes)
+    print("Exiting Election Scraper")
+    
 def election_scraper(url, output_file_name):
     validate_output_file_name(output_file_name)
     response = download_web_page(url, True)
     soup = BeautifulSoup(response.content, "lxml")
     region_urls = get_region_urls(soup)
-    process_data(region_urls)
+    process_data_and_make_file(region_urls, output_file_name)
     
 if __name__ == "__main__": 
     if not len(sys.argv) == 3:
